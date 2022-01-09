7d55b7cd8581f31e2bfac540ba13acd3854d90b4
Evans Djangbah <djangbahevans@yahoo.com>
2022-01-09 21:16:12 +0000

    Heavily modified ContactsTable

--- src/components/ContactsTable.tsx
+++ src/components/ContactsTable.tsx
@@ -1,99 +1,30 @@
 import DeleteIcon from '@mui/icons-material/Delete';
 import FilterListIcon from '@mui/icons-material/FilterList';
-import { Box, Checkbox, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, TableSortLabel, Toolbar, Tooltip, Typography } from "@mui/material"
+import { Box, Checkbox, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, Toolbar, Tooltip, Typography } from "@mui/material";
 import { alpha } from '@mui/material/styles';
-import { visuallyHidden } from '@mui/utils';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { ContactRow } from ".";
+import { IContact } from "../utils/sharedInterfaces";
 
-interface IContacts {
-  name: string;
-  email: string;
-  phoneNumber: string;
-  job: string;
-  notes: string;
+interface IContactsTableProps {
+  numSelected: number;
+  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  rowCount: number;
 }
 
-function createData(
-  name: string,
-  email: string,
-  phoneNumber: string,
-  job: string,
-  notes: string,
-): IContacts {
-  return {
-    name,
-    email,
-    phoneNumber,
-    job,
-    notes,
-  };
+interface IContactsTableToolbarProps {
+  numSelected: number;
 }
 
-const rows = [
-  createData('Aba Anoa Aidoo', "email@email.com", "+233547045920", "", ""),
-  createData('Abass Zakariya', "", "+233547045920", "", ""),
-  createData('Afua Asantewaa Asiedu', "", "+233547045920", "", ""),
-  createData('Abdul-Hamid Mohammed Nakobu', "", "+233547045920", "", ""),
-  createData('Abednego Appiah', "", "+233547045920", "", ""),
-  createData('Abednego Hagan', "", "+233547045920", "", ""),
-  createData('Dr Abena Tannor', "", "+233547045920", "", ""),
-  createData('Abigail Abena Konadu', "", "+233547045920", "", ""),
-  createData('Abigail Addo', "", "+233547045920", "", ""),
-  createData('Abigail Agyapong', "", "+233547045920", "", ""),
-  createData('Abigail Aruna', "", "+233547045920", "", ""),
-  createData('Abigail Bruce', "", "+233547045920", "", ""),
-  createData('Abigail Korkor Djangbah', "", "+233547045920", "", ""),
-  createData('Abigail Twenewaa', "", "+233547045920", "", ""),
-  createData('Abigail Yayra Fiagbe', "", "+233547045920", "", ""),
-];
-
-function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-type Order = 'asc' | 'desc';
-
-function getComparator<Key extends keyof any>(
-  order: Order,
-  orderBy: Key,
-): (
-    a: { [key in Key]: number | string },
-    b: { [key in Key]: number | string },
-  ) => number {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-// This method is created for cross-browser compatibility, if you don't
-// need to support IE11, you can use Array.prototype.sort() directly
-function stableSort<T>(array: readonly T[], comparator: (a: T, b: T) => number) {
-  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) {
-      return order;
-    }
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
-
-interface HeadCell {
+interface IHeadCell {
   disablePadding: boolean;
-  id: keyof IContacts;
+  id: keyof IContact;
   label: string;
 }
 
-const headCells: readonly HeadCell[] = [
+const headCells: readonly IHeadCell[] = [
   {
-    id: 'name',
+    id: 'given_name',
     disablePadding: true,
     label: 'Name',
   },
@@ -103,12 +34,12 @@ const headCells: readonly HeadCell[] = [
     label: 'Email',
   },
   {
-    id: 'phoneNumber',
+    id: 'phone1',
     disablePadding: false,
     label: 'Phone Number',
   },
   {
-    id: 'job',
+    id: 'organization',
     disablePadding: false,
     label: 'Job',
   },
@@ -119,22 +50,9 @@ const headCells: readonly HeadCell[] = [
   },
 ];
 
-interface ContactsTableProps {
-  numSelected: number;
-  onRequestSort: (event: React.MouseEvent<unknown>, property: keyof IContacts) => void;
-  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  order: Order;
-  orderBy: string;
-  rowCount: number;
-}
-
-function ContactsTableHead(props: ContactsTableProps) {
-  const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
+function ContactsTableHead(props: IContactsTableProps) {
+  const { onSelectAllClick, numSelected, rowCount } =
     props;
-  const createSortHandler =
-    (property: keyof IContacts) => (event: React.MouseEvent<unknown>) => {
-      onRequestSort(event, property);
-    };
 
   return (
     <TableHead>
@@ -155,20 +73,8 @@ function ContactsTableHead(props: ContactsTableProps) {
             key={headCell.id}
             align="left"
             padding='normal'
-            sortDirection={orderBy === headCell.id ? order : false}
           >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <Box component="span" sx={visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </Box>
-              ) : null}
-            </TableSortLabel>
+            {headCell.label}
           </TableCell>
         ))}
       </TableRow>
@@ -176,11 +82,7 @@ function ContactsTableHead(props: ContactsTableProps) {
   );
 }
 
-interface ContactsTableToolbarProps {
-  numSelected: number;
-}
-
-const ContactsTableToolbar = (props: ContactsTableToolbarProps) => {
+const ContactsTableToolbar = (props: IContactsTableToolbarProps) => {
   const { numSelected } = props;
 
   return (
@@ -231,36 +133,43 @@ const ContactsTableToolbar = (props: ContactsTableToolbarProps) => {
 };
 
 export default function ContactsTable() {
-  const [order, setOrder] = useState<Order>('asc');
-  const [orderBy, setOrderBy] = useState<keyof IContacts>('name');
-  const [selected, setSelected] = useState<readonly string[]>([]);
+  const [selected, setSelected] = useState<readonly number[]>([]);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [rows, setRows] = useState<IContact[]>([])
 
-  const handleRequestSort = (
-    event: React.MouseEvent<unknown>,
-    property: keyof IContacts,
-  ) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
-    setOrderBy(property);
-  };
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("http://127.0.0.1:8000/contacts", {
+        method: 'get',
+        mode: "cors",
+        // cache: "no-cache",
+        headers: {
+          "authorization": `Bearer ${localStorage.getItem("access_token")}`
+        }
+      })
+      const data: IContact[] = await response.json()
+      console.log(data)
+
+      setRows(data)
+    })()
+  }, [])
 
   const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.checked) {
-      const newSelecteds = rows.map((n) => n.name);
+      const newSelecteds = rows.map((n) => n.id);
       setSelected(newSelecteds);
       return;
     }
     setSelected([]);
   };
 
-  const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected: readonly string[] = [];
+  const handleClick = (event: React.MouseEvent<unknown>, id: number) => {
+    const selectedIndex = selected.indexOf(id);
+    let newSelected: readonly number[] = [];
 
     if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
+      newSelected = newSelected.concat(selected, id);
     } else if (selectedIndex === 0) {
       newSelected = newSelected.concat(selected.slice(1));
     } else if (selectedIndex === selected.length - 1) {
@@ -284,7 +193,7 @@ export default function ContactsTable() {
     setPage(0);
   };
 
-  const isSelected = (name: string) => selected.indexOf(name) !== -1;
+  const isSelected = (id: number) => selected.indexOf(id) !== -1;
 
   // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows =
@@ -302,54 +211,17 @@ export default function ContactsTable() {
           >
             <ContactsTableHead
               numSelected={selected.length}
-              order={order}
-              orderBy={orderBy}
               onSelectAllClick={handleSelectAllClick}
-              onRequestSort={handleRequestSort}
               rowCount={rows.length}
             />
             <TableBody>
-              {/* if you don't need to support IE11, you can replace the `stableSort` call with:
-              rows.slice().sort(getComparator(order, orderBy)) */}
-              {stableSort(rows, getComparator(order, orderBy))
+              {rows.slice()
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row, index) => {
-                  const isItemSelected = isSelected(row.name);
-                  const labelId = `contacts-table-checkbox-${index}`;
+                  const isItemSelected = isSelected(row.id);
 
                   return (
-                    <TableRow
-                      hover
-                      onClick={(event) => handleClick(event, row.name)}
-                      role="checkbox"
-                      aria-checked={isItemSelected}
-                      tabIndex={-1}
-                      key={row.name}
-                      selected={isItemSelected}
-                    >
-                      <TableCell padding="checkbox">
-                        <Checkbox
-                          color="primary"
-                          checked={isItemSelected}
-                          inputProps={{
-                            'aria-labelledby': labelId,
-                          }}
-                        />
-                      </TableCell>
-                      <TableCell
-                        component="th"
-                        id={labelId}
-                        scope="row"
-                        padding="none"
-                      >
-                        {row.name}
-                      </TableCell>
-                      <TableCell align="left">{row.email}</TableCell>
-                      <TableCell align="left">{row.phoneNumber}</TableCell>
-                      <TableCell align="left">{row.job}</TableCell>
-                      <TableCell align="left">{row.notes}</TableCell>
-                      <TableCell align="left"></TableCell>
-                    </TableRow>
+                    <ContactRow key={row.id} handleClick={handleClick} contact={row} isSelected={isItemSelected} />
                   );
                 })}
               {emptyRows > 0 && (
@@ -379,4 +251,3 @@ export default function ContactsTable() {
 }
 
 export { ContactsTable };
-
---
+++ src/utils/sharedInterfaces.tsx
@@ -0,0 +1,21 @@
+export type IContact = {
+  id: number
+  given_name?: string
+  additional_name?: string
+  family_name?: string
+  name_prefix?: string
+  name_suffix?: string
+  birthday?: string
+  gender?: string
+  location?: string
+  occupation?: string
+  notes?: string
+  photo?: string
+  email?: string
+  phone1?: string
+  phone2?: string
+  organization?: string
+  website?: string
+  createdAt: Date
+  user_id: number
+}
